9368cf742f00ebecd6de691f317484159ed8c6d6
AlbanMarku <albanmarku@outlook.com>
2022-10-14 16:53:26 +0100

    Print func but buggy.

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,6 +3,8 @@ import React from 'react';
 import './form.css';
 import uniqid from 'uniqid';
 import PropTypes from 'prop-types';
+import JsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 import PersonalDetail from './PersonalDetail/PersonalDetail';
 import ExperienceDetail from './ExperienceDetail/ExperienceDetail';
 
@@ -14,6 +16,14 @@ function Form({
       pos: '', workMonths: '', company: '', id: uniqid('expObj-'),
     }]);
   };
+  async function handlePdf() {
+    const selectedDiv = document.querySelector('.Preview');
+    const doc = new JsPDF('portrait', 'pt', 'a4');
+    await html2canvas(selectedDiv).then((canvas) => {
+      doc.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0);
+      doc.output('dataurlnewwindow');
+    });
+  }
   return (
     <div className="Form">
       <form>
@@ -32,6 +42,8 @@ function Form({
           <button type="button" onClick={handleClick}>Add comp</button>
         </div>
       </form>
+      <button type="button" onClick={handlePdf}>Print CV</button>
+
     </div>
   );
 }
